f6d47eefcd732567a345b1151cea1a461ea4350a
Phil Georgiou <philipposgeorgiou7@gmail.com>
2024-09-05 10:37:33 +0100

    fix: add catch blocks for error handling, add DB call to post route

--- controllers/home.js
+++ controllers/home.js
@@ -13,15 +13,17 @@ const getComponents = () => {
 
 const getHome = (req, res, next) => {
 
-    getComponents().then(components => {
+    getComponents()
+        .then(components => {
 
-        res.status(200).render("home", {
-            spec: {},
-            totalRetailCost: 0,
-            totalResellerCost: 0,
-            components,
-        });
-    });
+            res.status(200).render("home", {
+                spec: {},
+                totalRetailCost: 0,
+                totalResellerCost: 0,
+                components,
+            });
+        })
+        .catch(err => console.log(err));
 };
 
 const postHome = (req, res, next) => {
@@ -42,6 +44,8 @@ const postHome = (req, res, next) => {
         }
     };
 
+    // so that we can formulate totals for base components and resource components, respectively
+    // then add those up and pass the information to our view to be displayed
     const baseComponentCost = Object.values(spec.baseComponents).reduce((partialSum, a) => partialSum + a, 0);
     const resourceComponentCost = (4 * spec.resourceComponents.ram) + (3 * spec.resourceComponents.ssd) + (7 * spec.resourceComponents.gpu);
     const margin = 3500;
@@ -49,13 +53,17 @@ const postHome = (req, res, next) => {
     const totalRetailCost = (baseComponentCost + resourceComponentCost) + (1000 + margin);
     const totalResellerCost = (baseComponentCost + resourceComponentCost) + margin;
 
-    return res.status(200).render("home", {
-        spec,
-        totalRetailCost,
-        totalResellerCost,
-        components: []
+    getComponents()
+        .then(components => {
+            return res.status(200).render("home", {
+                spec,
+                totalRetailCost,
+                totalResellerCost,
+                components
 
-    });
+            });
+        })
+        .catch();
 
 };
 
