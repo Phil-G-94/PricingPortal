df5bc7d57b38f6c135c301158305f31859b0d95f
Phil Georgiou <philipposgeorgiou7@gmail.com>
2024-09-29 19:55:30 +0100

    pkg: add jwt, bcrypt. backend: add User model (untested). frontend: add signup and login pages, rendering corresponding components.

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -1,3 +1,8 @@
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import { validationResult } from "express-validator";
+import { User } from "../model/user.js";
+
 const putSignup = (req, res, next) => {
     res.status(201).json({
         message: "Signup successful",
