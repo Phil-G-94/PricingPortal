f860337f5ab77432271e35f24bb79b15ede49f72
Phil Georgiou <philipposgeorgiou7@gmail.com>
2024-12-08 18:38:43 +0000

    remove redundant code - backend

--- backend/middleware/isAuth.js
+++ backend/middleware/isAuth.js
@@ -40,32 +40,6 @@ const isAuth = (req, res, next) => {
         next(err);
     }
 
-    // const token = req.get("Authorization").split(" ")[1];
-    // let verifiedToken;
-
-    // if (!token) {
-    //     const error = new Error();
-    //     error.code = 401;
-    //     error.message = "No token present. User is not authorised.";
-    //     throw error;
-    // }
-
-    // try {
-    //     verifiedToken = jwt.verify(token, process.env.JWT_SECRET);
-    // } catch (err) {
-    //     if (!err.statusCode) {
-    //         err.statusCode = 500;
-    //         throw err;
-    //     }
-    // }
-
-    // if (!verifiedToken) {
-    //     const error = new Error("Not authenticated");
-    //     error.statusCode = 401;
-    //     throw error;
-    // }
-    // req.userId = verifiedToken.userId;
-    // next();
 };
 
 export { isAuth };
